f7c1fda6d9b31ed8fb14d499e00a6a2fa3d9136b
dacjames <dac@dac.io>
2014-07-19 14:31:49 -0700

    Special object for printing support

--- bootstrap/mara/special.py
+++ bootstrap/mara/special.py
@@ -2,6 +2,17 @@
 Define some special purpose singletons.
 '''
 
-NULL = object()
-UNIT = object()
-HALT = object()
+
+class SpecialValue(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return str(self.name)
+
+    def __repr__(self):
+        return str(self.name)
+
+NULL = SpecialValue('NULL')
+UNIT = SpecialValue('UNIT')
+HALT = SpecialValue('HALT')
